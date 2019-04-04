62064b6cf36e95d51e1075ebae0bd9b9c286dc9b
Ana Hernandez <her17138@uvg.edu.gt>
2019-04-04 17:55:45 -0600

    Initial commit

---
+++ principal.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Ana Lucia Hernandez
+# 17138
+# Graficas por Computadora 
+
+import shaders as s
+
+s.bm = s.Bitmap(1800,1800)
+#                                 translate    scale        eye       up      center      color     luz
+s.bm.load("esferatriangulada.obj", (0,0,0), (0.8,0.8,0.8), (1,1,1), (0,1,0), (0,0,0), (0,255,0), (1,1,1))
+s.glFinish("sphere")
